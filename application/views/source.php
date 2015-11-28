83ec6f32792cb8cab2c2803929433ae46e5bec82
王燕红 <wangyanhong@edaijia-inc.cn>
2015-11-28 19:21:45 +0800

    source内容修改为不引用模板的

--- application/views/source.php
+++ application/views/source.php
@@ -13,14 +13,11 @@
 		</select>
 		<a href="javascript:;" class="btn btn-green">搜索</a>
 	</div>
-	<div id="t"></div>
-	<script type="text/html" id="model">
-		{{each model}}
-		<div class="hd">{{$value.title}}</div>
+	<div id="t">
+		<div class="hd">title</div>
 		<div class="bd">
-			{{each $value.item}}
 			<div class="item">
-				<div class="tit">{{value}}教学视频-2015-12-12</div>
+				<div class="tit">教学视频-2015-12-12</div>
 				<div class="img">
 					<img src="../../frontend/img/banner01.jpg" alt="" />
 					<span class="mask">
@@ -32,14 +29,78 @@
 					资源技术资源技术资源技术资源技术资源技术资源技术
 				</div>
 			</div>
-			{{/each}}
+			<div class="item">
+				<div class="tit">教学视频-2015-12-12</div>
+				<div class="img">
+					<img src="../../frontend/img/banner01.jpg" alt="" />
+					<span class="mask">
+						<a href="javascript:;">查看</a>
+						<a href="javascript:;">下载</a>
+					</span>
+				</div>
+				<div class="con">
+					资源技术资源技术资源技术资源技术资源技术资源技术
+				</div>
+			</div>
+			<div class="item">
+				<div class="tit">教学视频-2015-12-12</div>
+				<div class="img">
+					<img src="../../frontend/img/banner01.jpg" alt="" />
+					<span class="mask">
+						<a href="javascript:;">查看</a>
+						<a href="javascript:;">下载</a>
+					</span>
+				</div>
+				<div class="con">
+					资源技术资源技术资源技术资源技术资源技术资源技术
+				</div>
+			</div>
 		</div>
-		{{/each}}
-	</script>
+		<div class="hd">title</div>
+		<div class="bd">
+			<div class="item">
+				<div class="tit">教学视频-2015-12-12</div>
+				<div class="img">
+					<img src="../../frontend/img/banner01.jpg" alt="" />
+					<span class="mask">
+						<a href="javascript:;">查看</a>
+						<a href="javascript:;">下载</a>
+					</span>
+				</div>
+				<div class="con">
+					资源技术资源技术资源技术资源技术资源技术资源技术
+				</div>
+			</div>
+			<div class="item">
+				<div class="tit">教学视频-2015-12-12</div>
+				<div class="img">
+					<img src="../../frontend/img/banner01.jpg" alt="" />
+					<span class="mask">
+						<a href="javascript:;">查看</a>
+						<a href="javascript:;">下载</a>
+					</span>
+				</div>
+				<div class="con">
+					资源技术资源技术资源技术资源技术资源技术资源技术
+				</div>
+			</div>
+			<div class="item">
+				<div class="tit">教学视频-2015-12-12</div>
+				<div class="img">
+					<img src="../../frontend/img/banner01.jpg" alt="" />
+					<span class="mask">
+						<a href="javascript:;">查看</a>
+						<a href="javascript:;">下载</a>
+					</span>
+				</div>
+				<div class="con">
+					资源技术资源技术资源技术资源技术资源技术资源技术
+				</div>
+			</div>
+		</div>
+	</div>
 
 	<script type="text/javascript" src="<?php echo base_url(); ?>frontend/js/jquery.min.js"></script>
-	<script type="text/javascript" src="<?php echo base_url(); ?>rontend/js/template.js"></script>
-	<script type="text/javascript" src="<?php echo base_url(); ?>frontend/js/source.js"></script>
 </body>
 </html>
 
