8a2e130790b4c5e3fefc8f6f466826c03f7a2218
王燕红 <wangyanhong@edaijia-inc.cn>
2015-11-30 13:19:55 +0800

    修改help的头部的帮助不可点击

--- frontend/js/head.js
+++ frontend/js/head.js
@@ -5,5 +5,18 @@
 * @version 	v0.0.1
 */
 $(function(){
-	
+	// 帮助点击事件
+	var $drop = $('div.drop','div.top');
+	$('div.top').on('click', 'li.help', function(e){
+		e.stopPropagation();
+		$(this).addClass('on').find('.drop').show();
+	}).on('click', 'div.drop a', function(e){
+		e.stopPropagation();
+		$drop.hide();
+		$('li.help').removeClass('on');
+	});
+	$('body').on('click', function(e){
+		$drop.hide();
+		$('li.help').removeClass('on');
+	});
 });
